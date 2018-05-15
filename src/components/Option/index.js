9a46dd261b68d039dd86438cec5b07c39d0a822d
Mansi Patil <mansi.p@sankeysolutions.com>
2018-05-15 19:04:32 +0530

    toggle-icon images updated

--- src/components/Option/index.js
+++ src/components/Option/index.js
@@ -5,6 +5,16 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import './styles.css';
 import boldOrange from '../../../images/typ-bold-orange.svg';
+import italicActive from '../../../images/typ_italic_active.svg';
+import leftActive from '../../../images/typ_align_left_active.svg';
+import centerActive from '../../../images/typ_align_centre_active.svg';
+import rightActive from '../../../images/typ_align_right_active.svg';
+import justifyActive from '../../../images/typ_align_justified_active.svg';
+import sizeplusActive from '../../../images/typ_size_increase_active.svg';
+import sizeminusActive from '../../../images/typ_size_decrease_active.svg';
+import unlinkActive from '../../../images/ui_link_minus_active.svg';
+import linkActive from '../../../images/ui_link_plus_active.svg';
+import underlineActive from '../../../images/typ_underline_active.svg';
 
 export default class Option extends Component {
   static propTypes = {
@@ -28,25 +38,25 @@ export default class Option extends Component {
     switch(value) {
       case 'bold': active ? this.setOptionIcon(boldOrange) : this.setOptionIcon(src);
         break;
-      case 'italic': active ? this.setOptionIcon(src) : this.setOptionIcon(src);
+      case 'italic': active ? this.setOptionIcon(italicActive) : this.setOptionIcon(src);
         break;
-      case 'underline': active ? this.setOptionIcon(src) : this.setOptionIcon(src);
+      case 'underline': active ? this.setOptionIcon(underlineActive) : this.setOptionIcon(src);
         break;
-      case 'left': active ? this.setOptionIcon(src) : this.setOptionIcon(src);
+      case 'left': active ? this.setOptionIcon(leftActive) : this.setOptionIcon(src);
         break;
-      case 'center': active ? this.setOptionIcon(src) : this.setOptionIcon(src);
+      case 'center': active ? this.setOptionIcon(centerActive) : this.setOptionIcon(src);
         break;
-      case 'right': active ? this.setOptionIcon(src) : this.setOptionIcon(src);
+      case 'right': active ? this.setOptionIcon(rightActive) : this.setOptionIcon(src);
         break;
-      case 'justify': active ? this.setOptionIcon(src) : this.setOptionIcon(src);
+      case 'justify': active ? this.setOptionIcon(justifyActive) : this.setOptionIcon(src);
         break;
-      case 'unordered': active ? this.setOptionIcon(src) : this.setOptionIcon(src);
+      case 'unordered': active ? this.setOptionIcon(sizeplusActive) : this.setOptionIcon(src);
         break;
-      case 'ordered': active ? this.setOptionIcon(src) : this.setOptionIcon(src);
+      case 'ordered': active ? this.setOptionIcon(sizeminusActive) : this.setOptionIcon(src);
         break;
-      case 'ordered-list-item': active ? this.setOptionIcon(src) : this.setOptionIcon(src);
+      case 'ordered-list-item': active ? this.setOptionIcon(unlinkActive) : this.setOptionIcon(src);
         break;
-      case 'unordered-list-item': active ? this.setOptionIcon(src) : this.setOptionIcon(src);
+      case 'unordered-list-item': active ? this.setOptionIcon(linkActive) : this.setOptionIcon(src);
         break;
       default:
         this.setOptionIcon(src);
